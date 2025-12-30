6e6aa190f0c58d424e1d84bd9d079f6bc2e8e6af
Yasen Abaker <yasenabker522@gmail.com>
2025-12-30 18:10:43 +0200

    add splash screens

--- lib/core/constants/image_strings.dart
+++ lib/core/constants/image_strings.dart
@@ -8,4 +8,9 @@ class YImageStrings {
   // -- Social Logos
   static const String google = "";
   static const String facebook = "";
+
+  // -- Onboarding images
+  static const String onboardingImage1 = "assets/images/onboarding_images/onboarding-image-one.gif";
+  static const String onboardingImage2 = "assets/images/onboarding_images/onboarding-image-two.gif";
+  static const String onboardingImage3 = "assets/images/onboarding_images/onboarding-image-three.gif";
 }
